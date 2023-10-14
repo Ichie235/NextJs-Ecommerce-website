ae5acf63026d6bfc5c08503f12233d811344544e
Ichie Chinemerem Richard <108372995+Ichie235@users.noreply.github.com>
2023-10-14 04:00:21 +0100

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from 'tailwindcss'
+import daisyui from "daisyui";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        lightTheme: {
+          primary: "#a4c92c",
+          secondary: "#bced8e",
+          accent: "#145189",
+          neutral: "#2a1c31",
+          "base-100": "#4f3357",
+          info: "#80c1ef",
+          success: "#51ecda",
+          warning: "#f18f0e",
+          error: "#ea6676",
+          body:{
+            "background-color":"#e3e6e6"
+          }
+        },
+      },
+    ],
+  },
+};
+export default config;
